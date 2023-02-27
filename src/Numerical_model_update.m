b50b6c08cab6a2bc83fbdcf92602761a7b8f6ad2
Adam Alexander Hempler-Christiansen <Adamalexander1997@gmail.com>
2023-02-27 14:02:46 +0100

    Export data for plots

--- src/Numerical_model_update.m
+++ src/Numerical_model_update.m
@@ -432,3 +432,35 @@ disp(strcat('Mode shape accuracy (MAC),4 : ',num2str(dmac(4)*100),'%'));
 disp(strcat('Mode shape accuracy (MAC),5 : ',num2str(dmac(5)*100),'%'));
 disp(strcat('Mean mode shape accuracy (MAC): ',num2str(mean(dmac)*100),'%'));
 disp('----------------------------------------------------------------------')
+
+if Stif == 1
+    save('.\data\costfunupdateSSIfreq.mat','OMAphi','OMAfreq','K','x','phi');
+elseif Stif == 2
+    save('.\data\costfunupdateERAfreq.mat','OMAphi','OMAfreq','K','x','phi');
+elseif Stif == 3
+    save('.\data\costfunupdateFDDfreq.mat','OMAphi','OMAfreq','K','x','phi');
+elseif Stif == 4
+    save('.\data\costfunupdateSSImode.mat','OMAphi','OMAfreq','K','x','phi');
+elseif Stif == 5
+    save('.\data\costfunupdateERAmode.mat','OMAphi','OMAfreq','K','x','phi');
+elseif Stif == 6
+    save('.\data\costfunupdateFDDmode.mat','OMAphi','OMAfreq','K','x','phi');
+elseif Stif == 7
+    save('.\data\costfunupdateSSIfreqmode.mat','OMAphi','OMAfreq','K','x','phi');
+elseif Stif == 8
+    save('.\data\costfunupdateERAfreqmode.mat','OMAphi','OMAfreq','K','x','phi');
+elseif Stif == 9
+    save('.\data\costfunupdateFDDfreqmode.mat','OMAphi','OMAfreq','K','x','phi');
+elseif Stif == 10
+    save('.\data\costfunupdateSSIfreqmodeEIL.mat','OMAphi','OMAfreq','K','x','phi');
+elseif Stif == 11
+    save('.\data\costfunupdateERAfreqmodeEIL.mat','OMAphi','OMAfreq','K','x','phi');
+elseif Stif == 12
+    save('.\data\costfunupdateFDDfreqmodeEIL.mat','OMAphi','OMAfreq','K','x','phi');
+elseif Stif == 13
+    save('.\data\costfunupdateSSIfreqmodeEILJAN.mat','OMAphi','OMAfreq','K','x','phi');
+elseif Stif == 14
+    save('.\data\costfunupdateERAfreqmodeEILJAN.mat','OMAphi','OMAfreq','K','x','phi');
+elseif Stif == 15
+    save('.\data\costfunupdateFDDfreqmodeEILJAN.mat','OMAphi','OMAfreq','K','x','phi');
+end
