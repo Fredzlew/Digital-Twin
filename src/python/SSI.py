14d69a638b49d00fa45c886a642afd463d3cfe24
Frederik Emil Serritzlew <123598253+Fredzlew@users.noreply.github.com>
2023-03-07 11:17:55 +0100

    Changes in SSI with use of pyOMA

--- src/python/SSI.py
+++ src/python/SSI.py
@@ -7,6 +7,8 @@ from scipy import signal
 import matplotlib.pyplot as plt
 import PyOMA as oma
 from matplotlib.backend_bases import MouseButton
+from os.path import dirname, join as pjoin
+import scipy.io as sio
 # Create a figure and a set of subplots
 #fig,ax = plt.subplots()
 
@@ -15,9 +17,10 @@ from matplotlib.backend_bases import MouseButton
    #print([event.xdata, event.ydata])
 
 # ======== PRE-PROCESSING =====================================================
-# To open a .txt file create a variable containing the path to the file
-_file = r"C:\Users\Frede\OneDrive - Danmarks Tekniske Universitet\Github\Digital-Twin\src\data\Anela\data_1_2_1.txt" # Path to the txt file
 
+# To open a .txt file create a variable containing the path to the file
+#_file = r"C:\Users\Frede\OneDrive - Danmarks Tekniske Universitet\Github\Digital-Twin\src\data\Anela\data_1_2_1.txt" # Path to the txt file
+"""
 # open the file with pandas and create a dataframe
 # N.B. whatchout for header, separator and remove time column if present
 data = pd.read_csv(_file, header=1, delim_whitespace=True, index_col=False) 
@@ -29,38 +32,37 @@ data = np.delete(data, 5, 1)
 
 # Swap the sensors because the sensor 1 is at the top
 data[:,[4,3,2,1,0]] = data[:,[0,1,2,3,4]]
+"""
 
 
-# to retrieve the example data 
-#data, (fex, FI_ex, xi_ex) = oma.Exdata()
+#Finding the file
+data_dir = pjoin(dirname(sio.__file__), r"C:\Users\Frede\OneDrive - Danmarks Tekniske Universitet\Github\Digital-Twin\src\data")
+data_sim = pjoin(data_dir, 'data_sim_newmark_jan.mat')
+
+# Loading simulated data instead:
+data = sio.loadmat(data_sim)
+data = np.transpose(data["dis_new"])
+
+
 
 # Sampling frequency
 fs = 100 # [Hz] Sampling Frequency
 
-"""
-# Using SciPy's signal module we can pre-process our data e.g. performing
-# decimation, trend removal and filtering. 
-# Detrend and decimate
-data = signal.detrend(data, axis=0) # Trend rmoval
-q = 5 # Decimation factor
-data = signal.decimate(data,  q, ftype='fir', axis=0) # Decimation
-fs = fs/q # [Hz] Decimated sampling frequency
-
-# Filter
-_b, _a = signal.butter(12, (0.3,6.5), fs=fs, btype='bandpass')
-filtdata = signal.filtfilt(_b, _a, data,axis=0) # filtered data
-
-"""
 # ======== ANALYSIS ===========================================================
 
 # Bind the button_press_event with the onclick() method
 #fig.canvas.mpl_connect('button_press_event', onclick)
 # Run SSI
-br = 15
+br = 80
 SSIcov = oma.SSIcovStaDiag(data, fs, br)
 
 # Frequencies ccoming from the stability diagram
-FreQ = [1.65612, 5.02666, 7.90311, 10.1157, 11.5903]
+# Anela
+#FreQ = [1.65612, 5.02666, 7.90311, 10.1157, 11.5903]
+# Jan IRF
+#FreQ = [1.74735, 5.18789, 8.1239, 10.3009, 11.6527]
+# Jan new
+FreQ = [1.7456, 5.14236, 7.95408, 9.96252, 11.1702]
 # Extract the modal properties
 Res_SSIcov = oma.SSIModEX(FreQ, SSIcov[1])
 
