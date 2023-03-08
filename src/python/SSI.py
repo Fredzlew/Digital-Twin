d4532bc52f00a502785c159bfd9943fcb5d176fb
chntj <chntj@sdfi.dk>
2023-03-08 23:51:01 +0100

    Changes to see what is plotting

--- src/python/SSI.py
+++ src/python/SSI.py
@@ -19,10 +19,11 @@ import scipy.io as sio
 # ======== PRE-PROCESSING =====================================================
 
 # To open a .txt file create a variable containing the path to the file
-_file = r"C:\Users\Frede\OneDrive - Danmarks Tekniske Universitet\Github\Digital-Twin\src\data\Anela\data_1_2_1.txt" # Path to the txt file
-
+#_file = r"C:\Users\Frede\OneDrive - Danmarks Tekniske Universitet\Github\Digital-Twin\src\data\Anela\data_1_2_1.txt" # Path to the txt file
+_file = r"C:\Users\Christina\OneDrive - Danmarks Tekniske Universitet (1)\Github\Digital-Twin\src\data\Anela\data_1_2_1.txt" # Path to the txt file
 # open the file with pandas and create a dataframe
 # N.B. whatchout for header, separator and remove time column if present
+
 data = pd.read_csv(_file, header=1, delim_whitespace=True, index_col=False) 
 data = data.to_numpy()
 
@@ -51,8 +52,8 @@ fs = 100 # [Hz] Sampling Frequency
 # Bind the button_press_event with the onclick() method
 #fig.canvas.mpl_connect('button_press_event', onclick)
 # Run SSI
-br = 130
-SSIcov = oma.SSIcovStaDiag(data, fs, br)
+br = 15
+SSIcov,Result = oma.SSIcovStaDiag(data, fs, br)
 
 # Frequencies ccoming from the stability diagram
 # Anela
@@ -62,7 +63,7 @@ FreQ = [1.65612, 5.02666, 7.90311, 10.1157, 11.5903]
 # Jan new
 #FreQ = [1.7473, 5.18712, 8.12024, 10.2972, 11.6468]
 # Extract the modal properties
-Res_SSIcov = oma.SSIModEX(FreQ, SSIcov[1])
+Res_SSIcov = oma.SSIModEX(FreQ, Result)
 
 # =============================================================================
 # Make some plots
