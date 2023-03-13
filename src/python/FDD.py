3b2bc06084d2f56999e756df253443223c50bc76
Frederik Emil Serritzlew <123598253+Fredzlew@users.noreply.github.com>
2023-03-13 16:08:40 +0100

    small changes in data

--- src/python/FDD.py
+++ src/python/FDD.py
@@ -1,9 +1,3 @@
-# -*- coding: utf-8 -*-
-"""
-Created on Mon Mar 13 13:05:29 2023
-
-@author: Frede
-"""
 
 # Import modules
 import numpy as np
@@ -25,46 +19,46 @@ import scipy.io as sio
 # ======== PRE-PROCESSING =====================================================
 
 # To open a .txt file create a variable containing the path to the file
-#_file = r"C:\Users\Frede\OneDrive - Danmarks Tekniske Universitet\Github\Digital-Twin\src\data\Anela\data_1_2_1.txt" # Path to the txt file
+_file = r"C:\Users\Frede\OneDrive - Danmarks Tekniske Universitet\Github\Digital-Twin\src\data\Anela\data_1_2_1.txt" # Path to the txt file
 #_file = r"C:\Users\User\OneDrive\Dokumenter\GitHub\Digital-Twin\src\data\Anela\data_1_2_1.txt" # Path to the txt file
 #_file = r"C:\Users\Christina\OneDrive - Danmarks Tekniske Universitet (1)\Github\Digital-Twin\src\data\Anela\data_1_2_1.txt" # Path to the txt file
 # open the file with pandas and create a dataframe
 # N.B. whatchout for header, separator and remove time column if present
 
-#data = pd.read_csv(_file, header=1, delim_whitespace=True, index_col=False) 
-#data = data.to_numpy()
+data = pd.read_csv(_file, header=1, delim_whitespace=True, index_col=False) 
+data = data.to_numpy()
 
 # Removing the time column 
-#data = np.delete(data, 0, 1)
-#data = np.delete(data, 5, 1)
+data = np.delete(data, 0, 1)
+data = np.delete(data, 5, 1)
 
 # Swap the sensors because the sensor 1 is at the top
-#data[:,[4,3,2,1,0]] = data[:,[0,1,2,3,4]]
+data[:,[4,3,2,1,0]] = data[:,[0,1,2,3,4]]
 
 #Finding the file
-data_dir = pjoin(dirname(sio.__file__), r"C:\Users\Frede\OneDrive - Danmarks Tekniske Universitet\Github\Digital-Twin\src\data")
+#data_dir = pjoin(dirname(sio.__file__), r"C:\Users\Frede\OneDrive - Danmarks Tekniske Universitet\Github\Digital-Twin\src\data")
 #data_dir = pjoin(dirname(sio.__file__), r"C:\Users\User\OneDrive\Dokumenter\GitHub\Digital-Twin\src\data")
-data_sim = pjoin(data_dir, 'data_sim_newmark_jan.mat')
+#data_sim = pjoin(data_dir, 'data_sim_newmark_jan.mat')
 
 # Loading simulated data instead:
-data = sio.loadmat(data_sim)
-data = np.transpose(data["dis_new"])
+#data = sio.loadmat(data_sim)
+#data = np.transpose(data["dis_new"])
 
 
 
 # Sampling frequency
-fs = 1000 # [Hz] Sampling Frequency
+fs = 100 # [Hz] Sampling Frequency
 
 # Using SciPy's signal module we can pre-process our data e.g. performing
 # decimation, trend removal and filtering. 
 # Detrend and decimate
 data = signal.detrend(data, axis=0) # Trend rmoval
-q = 10 # Decimation factor
+q = 2 # Decimation factor
 data = signal.decimate(data,  q, ftype='fir', axis=0) # Decimation
 fs = fs/q # [Hz] Decimated sampling frequency
 
 # Filter
-_b, _a = signal.butter(12, (0.3,6.5), fs=fs, btype='bandpass')
+_b, _a = signal.butter(10, (0.3,6.5), fs=fs, btype='bandpass')
 filtdata = signal.filtfilt(_b, _a, data,axis=0) # filtered data
 
 # ======== ANALYSIS ===========================================================
