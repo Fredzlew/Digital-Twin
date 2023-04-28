0d07cda73798adcc10d65bc843a79a4f021c2eac
Adam Alexander Hempler-Christiansen <Adamalexander1997@gmail.com>
2023-04-28 10:37:24 +0200

    Update Cases - Kopi.m

--- src/Mottershead/Cases_copy.m
+++ src/Mottershead/Cases_copy.m
@@ -1,3 +1,4 @@
+clear;clc;close all
 %%%%%%%%%%%%%%
 %%% CASE 1 %%%
 %%%%%%%%%%%%%%
@@ -26,10 +27,7 @@ G = [um(1),um(1)-um(2),0;0,-um(1)+um(2),um(2)];
 con = cond(G);
 
 % symmetric weighting matrix
-Weps = (diag(fm))^-2;
-Weps = [1,0;0,7.65]
-Weps = [1/2,0;0,7.65/2]
-%Weps = (1/norm(r)^2)*(r*r') 
+Weps = [1/2,0;0,7.65/2];
 
 % The parameter weighting matrix for regularization problem
 x = diag(G'*Weps*G);
@@ -41,9 +39,6 @@ lambda = 10^-2;
 
 % the difference with regularization
 Deltatheta = ((G'*Weps*G)+(lambda^2*Wtheta))^(-1)*G'*Weps*r
-
-% The difference with parameter estimation
-%Deltatheta = (G'*Weps*G)^-1*G'*Weps*r;
 
 %%
 %%%%%%%%%%%%%%
@@ -74,8 +69,7 @@ G = [um(1),um(1)-um(2),0;0,-um(1)+um(2),um(2)];
 con = cond(G);
 
 % symmetric weighting matrix
-Weps = [1/2,0;0,7.65/2]
-%Weps = (1/norm(r)^2)*(r*r') 
+Weps = [1/2,0;0,7.65/2];
 
 % The parameter weighting matrix for regularization problem
 x = diag(G'*Weps*G);
@@ -87,58 +81,3 @@ lambda = 10^-2;
 
 % the difference with regularization
 Deltatheta = ((G'*Weps*G)+(lambda^2*Wtheta))^(-1)*G'*Weps*r
-
-% The difference with parameter estimation
-%Deltatheta = (G'*Weps*G)^-1*G'*Weps*r;
-%%
-%%%%%%%%%%%%%%
-%%% CASE 2 %%%
-%%%%%%%%%%%%%%
-% force vector
-fm1 = [1, 0]';
-fm2 = [0, 1]';
-fm = [fm1,fm2];
-
-
-% measured static deflection
-um1 = [0.60811;0.27027];
-um2 = [0.27027;0.67568];
-um = [um1,um2];
-
-% stiffness parameters in the initial model
-k1 = 1;
-k2 = 1;
-k3 = 1;
-
-% stiffness matrix
-K = [k1+k2,-k2;-k2,k2+k3];
-
-% input force residual
-r = fm - K*um;
-
-% The sensitivity matrix:
-G = [um(1),um(1)-um(2),0;0,-um(1)+um(2),um(2)];
-
-
-
-% condition number of G
-con = cond(G);
-
-% symmetric weighting matrix
-%Weps = (diag(fm))^-2;
-Weps = [1,0;0,1];
-%Weps = (1/norm(r)^2)*(r*r') 
-
-% The parameter weighting matrix for regularization problem
-x = diag(G'*Weps*G);
-Gamma = [x(1),0,0;0,x(2),0;0,0,x(3)];
-Wtheta = mean(diag(Gamma))/mean(diag(Gamma^-1))*Gamma^-1;
-
-% plotting to find the regularisation parameter:
-lambda = 10^-2;  
-
-% the difference with regularization
-Deltatheta = ((G'*Weps*G)+(lambda^2*Wtheta))^(-1)*G'*Weps*r;
-
-% The difference with parameter estimation
-Deltatheta = (G'*Weps*G)^-1*G'*Weps*r
