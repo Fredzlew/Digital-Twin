4a7911e5a7a913ab43c33b794cefc39cdfdbd844
Adam Alexander Hempler-Christiansen <Adamalexander1997@gmail.com>
2023-05-30 12:31:57 +0200

    Remove files and update virtual sensing

--- src/Virtual_Sensing/Virtual_sensing_MDE_val.m
+++ src/Virtual_Sensing/Virtual_sensing_MDE_val.m
@@ -5,9 +5,13 @@ clear;clc;close all
 addpath(genpath('data_sens'))
 
 % Load data
-data = readmatrix('data_1_2_1.txt')'; % Loading displacement data
+% High damping
+data = readmatrix('data_5_2_1.txt')'; % Loading displacement data
+% Low damping
+% data = readmatrix('data_5_6_1.txt')'; % Loading displacement data
+
 fss = data(2:6,:)/1000; % Converting mm to m
-f = [fss(5,:);fss(4,:);fss(3,:);fss(2,:);fss(1,:)]; % Swap columns due to sensor
+xm = [fss(5,:);fss(4,:);fss(3,:);fss(2,:);fss(1,:)]; % Swap columns due to sensor
 filename = load('Eigenvalue_modeshape_residual_stiffmass.mat');
 U = filename.U;
 % U = [0.3164, 0.7748, 1.0000, 1.0000, -0.4440;...
@@ -21,50 +25,42 @@ U = filename.U;
 %% Virtual sensing part
 %close all;
 % Number of modeshapes included in approximation
-num_ms = 2;
+num_ms = 3;
 
 % Index of measured locations (1 = bottom, 5 = top)
 im = [3,4,5];
 
 % Calculate displacement at predicted locations
-[xp] = VirtualSensVal(f,U,num_ms,im);
+[xp] = VirtualSensVal(xm,U,num_ms,im);
+
+% Number of time steps to plot
+nt = 4320000;
+
+% Show displacements for # virtual sensor (1 = bottom)
+vs = 1;
 
 % Plot actual displacements vs predicted displacements
 figure
 hold on
-if sum(abs(xp(2,:)))>sum(abs(f(2,:)))
-    plot(data(1,1:60000),xp(2,1:60000),'r')
-    plot(data(1,1:60000),f(2,1:60000),'b')
+if sum(abs(xp(1,:)))>sum(abs(xm(1,:)))
+    plot(data(1,1:nt),xp(vs,1:nt),'r')
+    plot(data(1,1:nt),xm(vs,1:nt),'b')
     legend('Predicted displacements','Actual displacements')
 else
-    plot(data(1,1:60000),f(2,1:60000),'b')
-    plot(data(1,1:60000),xp(2,1:60000),'r')
+    plot(data(1,1:nt),xm(vs,1:nt),'b')
+    plot(data(1,1:nt),xp(vs,1:nt),'r')
     legend('Actual displacements','Predicted displacements')
 end
 hold off
-title('Displacements at first virtual sensor')
+title('Displacements at virtual sensor')
 xlabel('Time [s]')
 ylabel('Displacement [m]')
 
-% Plot the relative error between actual displacements and predicted displacements
-% err = zeros(size(xp,1),size(xp,2));
-% for j = 1:size(xp,1)
-%     for i = 1:size(xp,2)
-%         err(j,i) = abs(abs(xp(j,i))-abs(f(j,i)))/abs(f(j,i))*100;
-%     end
-% end
-% 
-% figure (2)
-% plot(err(1,:))
-% title('Relative error')
-% xlabel('Time step [-]')
-% ylabel('Error [%]')
+%%% Calculate quality indicators %%%
+% TRAC
+TRAC = (xm(vs,1:nt)*xp(vs,1:nt)')^2/(xm(vs,1:nt)*xm(vs,1:nt)'*xp(vs,1:nt)*xp(vs,1:nt)');
+disp(['TRAC value: ',num2str(TRAC)])
 
-
-
-
-
-
-
-
-
+% MAE (in meters)
+MAE = sum(abs(xm(vs,1:nt)-xp(vs,1:nt)))/nt;
+disp(['MAE value: ',num2str(MAE)])
