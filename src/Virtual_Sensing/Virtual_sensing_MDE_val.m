af8a38b65a4082961df0329a4a6736873d372faf
Adam Alexander Hempler-Christiansen <Adamalexander1997@gmail.com>
2023-06-05 09:34:43 +0200

    Virtual sens with simulated data

--- src/Virtual_Sensing/Virtual_sensing_MDE_val.m
+++ src/Virtual_Sensing/Virtual_sensing_MDE_val.m
@@ -6,15 +6,27 @@ addpath(genpath('data_sens'))
 
 % Load data
 % High damping
-data = readmatrix('data_5_2_1.txt')'; % Loading displacement data
+% data = readmatrix('data_5_2_1.txt')'; % Loading displacement data
 % Low damping
 % data = readmatrix('data_5_6_1.txt')'; % Loading displacement data
+% filename = load('Eigenvalue_modeshape_residual_stiffmass.mat');
+% U = filename.U;
+% fss = data(2:6,:)/1000; % Converting mm to m
+% xbase = data(7,:)/1000;
+% xm = [fss(5,:);fss(4,:);fss(3,:);fss(2,:);fss(1,:)]-xbase; % Swap columns due to sensor
+% Simulated data
+file = load('data_sim_newmark_jan_damp.mat');
+xm = file.dis_new;
+% Modeshapes for simulated data
+U = [ 0.2448    0.6732    1.0000    1.0000    0.4947;...
+    0.5318    1.0000    0.3599   -0.7952   -0.8160;...
+    0.7614    0.5670   -0.9105   -0.1746    1.0000;...
+    0.9197   -0.2532   -0.5326    0.9118   -0.8649;...
+    1.0000   -0.8661    0.7245   -0.5096    0.3364];
+% Manually create timesteps
+data = linspace(0,2300,2300000);
 
-fss = data(2:6,:)/1000; % Converting mm to m
-xbase = data(7,:)/1000;
-xm = [fss(5,:);fss(4,:);fss(3,:);fss(2,:);fss(1,:)]-xbase; % Swap columns due to sensor
-filename = load('Eigenvalue_modeshape_residual_stiffmass.mat');
-U = filename.U;
+% Modeshapes from mottershead for High damping data
 % U = [0.2338    0.6457    1.0000    1.0000   -0.6471;...
 %     0.5108    1.0000    0.4949   -0.6345    1.0000;...
 %     0.7398    0.6738   -0.8045   -0.4821   -0.9794;...
@@ -29,13 +41,13 @@ U = filename.U;
 num_ms = 3;
 
 % Index of measured locations (1 = bottom, 5 = top)
-im = [2,4,5];
+im = [3,4,5];
 
 % Calculate displacement at predicted locations
 [xp,qt] = VirtualSensVal(xm,U,num_ms,im);
 
 % Number of time steps to plot
-nt = 100;
+nt = 10000;
 
 % Show displacements for # virtual sensor (1 = bottom)
 vs = 1;
