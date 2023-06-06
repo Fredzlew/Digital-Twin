80629d1a19968ef96a583cfeeedbf7b0642dfd95
Frederik Emil Serritzlew <123598253+Fredzlew@users.noreply.github.com>
2023-06-06 14:38:12 +0200

    Updated virtual sensing

--- src/Virtual_Sensing/Virtual_sensing_MDE_val.m
+++ src/Virtual_Sensing/Virtual_sensing_MDE_val.m
@@ -19,7 +19,7 @@ addpath(genpath('data_sens'))
 file = load('..\data\1_data_sim_newmark_jan.mat');
 xm = file.dis_new;
 filename = load('..\data\modelprop_jan.mat');
-U = filename.U,1;
+U = filename.U;
 
 % Manually create timesteps (stepsize dt=0.001)
 data = linspace(0,size(xm,2)*0.001,size(xm,2));
@@ -36,7 +36,7 @@ data = linspace(0,size(xm,2)*0.001,size(xm,2));
 %% Virtual sensing part
 %close all;
 % Number of modeshapes included in approximation (max length(im))
-num_ms = 2;
+num_ms = 3;
 
 % Index of measured locations (1 = bottom, 5 = top)
 im = [3,4,5];
@@ -48,7 +48,7 @@ im = [3,4,5];
 nt = 1000;
 
 % Show displacements for # virtual sensor (1 = bottom)
-vs = 1;
+vs = 3;
 
 % Plot actual displacements vs predicted displacements
 figure
@@ -56,24 +56,24 @@ hold on
 if sum(abs(xp(1,:)))>sum(abs(xm(1,:)))
     plot(data(1,1:nt),xp(vs,1:nt),'r')
     plot(data(1,1:nt),xm(vs,1:nt),'b')
-    legend('Predicted displacements','Actual displacements')
+    legend('Predicted displacements','Actual displacements','FontSize',14)
 else
     plot(data(1,1:nt),xm(vs,1:nt),'b')
     plot(data(1,1:nt),xp(vs,1:nt),'r')
-    legend('Actual displacements','Predicted displacements')
+    legend('Actual displacements','Predicted displacements','FontSize',14)
 end
 hold off
-title(['Displacements at virtual sensor:',num2str(vs)])
-xlabel('Time [s]')
-ylabel('Displacement [m]')
+title(['Displacements at virtual sensor:',num2str(vs)],'FontSize',20)
+xlabel('Time [s]','FontSize',14)
+ylabel('Displacement [m]','FontSize',14)
 
 %%% Calculate quality indicators %%%
 % TRAC
-TRAC = (xm(vs,1:nt)*xp(vs,1:nt)')^2/((xm(vs,1:nt)*xm(vs,1:nt)')*(xp(vs,1:nt)*xp(vs,1:nt)'));
+TRAC = (xm(vs,:)*xp(vs,:)')^2/((xm(vs,:)*xm(vs,:)')*(xp(vs,:)*xp(vs,:)'));
 disp(['TRAC value for sensor ',num2str(vs),' with ',num2str(num_ms),' modes:',num2str(TRAC)])
 
 % MAE (normalized with respect to the standard deviation)
-MAE = sum(abs(xm(vs,1:nt)-xp(vs,1:nt)))/nt/std(xp(vs,1:nt));
+MAE = sum(abs(xm(vs,:)-xp(vs,:)))/nt/std(xp(vs,:));
 disp(['MAE value for sensor ',num2str(vs),' with ',num2str(num_ms),' modes:',num2str(MAE)])
 
 % Plot the modal coordinates in the frequency domain
